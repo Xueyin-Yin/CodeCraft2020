046f02ec0b8303d42ecd86f4ad737ef28bf29038
Xueyin-Yin <48113085+Xueyin-Yin@users.noreply.github.com>
2020-04-23 00:33:38 -0400

    Add graph data structure

--- main.cpp
+++ main.cpp
@@ -3,16 +3,20 @@
 #include <cstring>
 #include <vector>
 #include <stack>
+#include <map>
+#include <set>
 
 using namespace std;
 
 #define INPUT_PATH "/data/test_data.txt"
 #define OUTPUT_PATH "/projects/student/result.txt"
 
+map<int, set<int>> graph;
+
 int tarjan(); // Find all strongly-connected components
 int buildGraph(); // Build graph based on the input file
 int writeResult(); // Write result to the output file
 
 int main(int argc, char* argv[]) {
-
+    return 0;
 }
