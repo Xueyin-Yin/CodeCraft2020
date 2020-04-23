4422eda0133cb8294f9bb9b4153c2a1cc9170f27
Hantp <hantsepiao@icloud.com>
2020-04-23 19:32:05 -0400

    translate dfs and dfs2 from python to cpp

--- main.cpp
+++ main.cpp
@@ -43,6 +43,62 @@ int buildGraph() {
 
 }
 
+void dfs(vector<vector<int>>& graph, int current_node, int root_node, vector<int>& visit, vector<vector<int>>& res, vector<int>& path)
+{
+    for(int i=0 ; i<graph[current_node].size() ; i++)
+    {
+        int next_node = graph[current_node][i];
+        if(next_node < root_node)
+        {
+            continue;
+        }
+        else if(visit1[next_node] == -2 && visit[next_node] == 0)
+        {
+            path.push_back(next_node);
+            int path_length = path.size();
+            if(path_length > 2)
+            {
+                res[path_length - 3].push_back(path);
+            }
+            path.pop_back();
+        }
+        else if(visit[next_node] == 1 || (visit1[next_node] != root_node && visit1[next_node] != -2))
+        {
+            continue;
+        }
+        else if(path.size() == 6 || next_node == root_node)
+        {
+            continue;
+        }
+
+        visit[next_node] = 1;
+        path.push_back(next_node);
+        dfs(graph, next_node, root_node, visit, visit1, res, path);
+        path.pop_back();
+        visit[next_node] = 0;
+    }
+}
+
+void dfs1(vector<vector<int>>& graph, int current_node, int root_node, vector<int>& visit, vector<int>& visit1, int length)
+{
+    for(int i=0 ; i<graph[k].size() ; i++)
+    {
+        int next_node = graph[current_node][i];
+        if(next_node < root_node || visit[next_node] == 1)
+        {
+            continue;
+        }
+
+        visit1[next_node] = root_node;
+        if(length == 3)
+            continue;
+
+        visit[next_node] = 1;
+        dfs1(graph, next_node, root_node, visit, visit1, length + 1);
+        visit[next_node] = 0;
+    }
+}
+
 int main(int argc, char* argv[]) {
 
     buildGraph();
